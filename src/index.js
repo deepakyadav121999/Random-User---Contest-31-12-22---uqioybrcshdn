c14ee6825144aafc05fba59d6d8fe13060df0ef1
deepakyadav121999 <113125064+deepakyadav121999@users.noreply.github.com>
2023-01-09 10:39:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+let disp = document.getElementById("blank");
+
+async function getRandomUSer(){
+    const response =await fetch('https://randomuser.me/api/');
+   const data = await response.json();
+   const user = data.results[0];
+
+   display(user);
+}
+ 
+function display(user){
+let fname = document.getElementById('fname')
+fname.innerText =`${user.name.first + " "+ user.name.last}`
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ let img =document.getElementById('mg');
+ img.setAttribute('src',`${user.picture.large}`)
+   
+ let age = document.getElementById("age");
+ age.addEventListener('click',()=>{
+    disp.innerHTML =`${user.dob.age}`
+
+ })
+ let email = document.getElementById("email");
+ email.addEventListener('click',()=>{
+    disp.innerText =`${user.email}`
+ })
+ let phone = document.getElementById("phone");
+ phone.addEventListener('click',()=>{
+   disp.innerText =`${user.phone}`
+ })
+  
+}
+getRandomUSer();
+let newu = document.getElementById("getUser");
+newu.addEventListener('click',()=>{
+getRandomUSer()
+ disp.innerText ="";
+})
